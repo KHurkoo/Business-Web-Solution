e04698faa86de1b4462719572f40dba83d2c287e
github username <t.gunness35@gmail.com>
2023-05-11 14:06:14 +0400

    update

--- chatgpt.js
+++ chatgpt.js
@@ -28,7 +28,7 @@ function Send() {
   }
   var sBody = JSON.stringify({
     "prompt": sQuestion + "\nAI:",
-    "max_tokens": 150,
+    "max_tokens": 1500,
     "temperature": 0.9,
     "top_p": 1,
     "n": 1,
@@ -47,27 +47,25 @@ function ChangeLang(oSel) {
   if (sMsg == "") {
     return;
   }
-  txtOutput.value = "Thinking...";
+  btnSend.disabled = true;
+  txtMsg.disabled = true;
+  selLang.disabled = true;
+  txtOutput.value = "Translating...";
   var oHttp = new XMLHttpRequest();
-  oHttp.open("POST", "https://api.openai.com/v1/engines/davinci/completions");
-  oHttp.setRequestHeader("Accept", "application/json");
+  oHttp.open("POST", "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=" + sLang, true);
   oHttp.setRequestHeader("Content-Type", "application/json");
-  oHttp.setRequestHeader("Authorization", "Bearer " + api);
+  oHttp.setRequestHeader("Ocp-Apim-Subscription-Key", "c0c5c0c5c0c5c0c5c0c5c0c5c0c5c0c5");
   oHttp.onreadystatechange = function() {
     if (oHttp.readyState == 4 && oHttp.status == 200) {
       var oResult = JSON.parse(oHttp.responseText);
-      txtOutput.value = oResult.choices[0].text;
+      txtMsg.value = oResult[0].translations[0].text;
+      btnSend.disabled = false;
+      txtMsg.disabled = false;
+      selLang.disabled = false;
     }
   }
-  var sBody = JSON.stringify({
-    "prompt": sMsg + "\nAI:",
-    "max_tokens": 150,
-    "temperature": 0.9,
-    "top_p": 1,
-    "n": 1,
-    "stream": false,
-    "logprobs": null,
-    "stop": "\n"
-  });
+  var sBody = JSON.stringify([{
+    "Text": sMsg
+  }]);
   oHttp.send(sBody);
 }
